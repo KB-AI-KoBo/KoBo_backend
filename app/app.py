c55efba1d4885080d9b086e65e549ec8da49e5dc
Park, seun <seunpark04@gmail.com>
2024-09-02 22:30:13 +0900

    Feat: app.py not query 예외 처리

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ CORS(app)  # CORS 설정
 llm = ChatOpenAI(temperature=0.5, model='gpt-4o', openai_api_key=openai_api_key)
 
 # AI 서비스 요청 처리 엔드포인트
-@app.route('/', methods=['POST','GET'])
+@app.route('/chat', methods=['POST','GET'])
 def process_request():
 
     # 외부 API로부터 JSON 데이터 가져오기
@@ -71,15 +71,15 @@ def process_request():
 
         # 결과를 Java 백엔드로 반환
         analysis_result = {
-		        "documentId": documentId,
-		        "questionContent": query,
-		        "result": result
-		    }
+            "documentId": documentId,
+            "questionContent": query,
+            "result": result
+        }
         request.post(f'{BACKEND_URL}/analysis/result',json = analysis_result)
         request.post(f'{BACKEND_URL}/support-programs',json = extracted_data)
 
         return jsonify({"status": "success"}), 200
-    
+
     except Exception as e:
         print(f"Error processing request: {e}")
         return jsonify({"error": "Internal server error", "details": str(e)}), 500
