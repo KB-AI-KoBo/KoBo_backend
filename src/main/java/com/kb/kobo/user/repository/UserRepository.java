fb90e2dd9c8df45ee6c33c08561a9a04361afd70
Park, seun <seunpark04@gmail.com>
2024-11-24 19:12:31 +0900

    refactor: 필요없는 메소드 삭제

--- src/main/java/com/kb/kobo/user/repository/UserRepository.java
+++ src/main/java/com/kb/kobo/user/repository/UserRepository.java
@@ -9,24 +9,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
-
-    // 등록 번호 기준
     Optional<User> findByRegistrationNumber(String registrationNumber);
-
     Optional<User> findByCompanyEmail(String companyEmail);
-
-    long countByIndustry(String industry);
-
-    List<User> findByCompanySize(String companySize);
-
-    List<User> findTop10ByOrderByCreatedAtDesc();
-
-    void deleteByCompanyName(String companyName);
-
-
-    List<User> findByCompanyEmailContaining(String companyEmail);
 }
