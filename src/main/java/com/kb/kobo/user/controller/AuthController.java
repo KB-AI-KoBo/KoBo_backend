42c0165f9310544dc6f5b75781024cc1f6dc85fc
Park, seun <seunpark04@gmail.com>
2024-11-25 14:04:06 +0900

    refactor: 로그인 컨트롤러 작성

--- src/main/java/com/kb/kobo/user/controller/AuthController.java
+++ src/main/java/com/kb/kobo/user/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.kb.kobo.user.controller;
 
+import com.kb.kobo.user.dto.TokenDto;
+import com.kb.kobo.user.dto.UserLoginReqDto;
+import com.kb.kobo.user.service.AuthService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,6 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public TokenDto login (@RequestBody UserLoginReqDto userLoginReqDto) {
+        return authService.login(userLoginReqDto);
+    }
 
 
 }
