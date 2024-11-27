41cf4a46cb766f003b960d2a48d7fee9dfda32a5
Park, seun <seunpark04@gmail.com>
2024-11-27 16:53:51 +0900

    chore: TokenDto에 Json 직렬화를 하여 406 에러 해결

--- src/main/java/com/kb/kobo/user/dto/TokenDto.java
+++ src/main/java/com/kb/kobo/user/dto/TokenDto.java
@@ -1,11 +1,11 @@
 package com.kb.kobo.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.*;
 import com.google.gson.annotations.SerializedName;
 
 @Builder
 @AllArgsConstructor
+@Data
 public class TokenDto {
 
     @SerializedName("access_token")
