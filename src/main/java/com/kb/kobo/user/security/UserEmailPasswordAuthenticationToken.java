b66bb180b9ff3d8d2b70381fa48ee66090b19b18
Park, seun <seunpark04@gmail.com>
2024-11-28 10:12:27 +0900

    UserDetailsServiceConfig 사용하지 않는 메소드 삭제

--- src/main/java/com/kb/kobo/user/security/UserEmailPasswordAuthenticationToken.java
+++ src/main/java/com/kb/kobo/user/security/UserEmailPasswordAuthenticationToken.java
@@ -7,12 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class UserEmailPasswordAuthenticationToken extends UsernamePasswordAuthenticationToken {
-
     public UserEmailPasswordAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
         super(principal, credentials, authorities);
-    }
-
-    public UserDetails getUserDetails() {
-        return (UserDetails) getPrincipal();
     }
 }
