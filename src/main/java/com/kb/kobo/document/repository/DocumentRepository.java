b0b051ce56e1da6e2a586e1b36e2423791673cc9
Park, seun <seunpark04@gmail.com>
2024-12-01 19:49:24 +0900

    refact: Document 리팩토링 - 로직 상의 변경사항은 없음

--- src/main/java/com/kb/kobo/document/repository/DocumentRepository.java
+++ src/main/java/com/kb/kobo/document/repository/DocumentRepository.java
@@ -1,14 +1,13 @@
 package com.kb.kobo.document.repository;
 
-import com.kb.kobo.user.domain.User;
 import com.kb.kobo.document.domain.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-
-    List<Document> findByUser(User user);
+    Optional<Document> findByDocumentId(Long documentId);
+    List<Document> findByUserId(Long userId);
 }
