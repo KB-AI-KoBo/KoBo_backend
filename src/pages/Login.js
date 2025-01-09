2057b9d5bb8f1f17b6483e3f9112bfd43471d8be
ming9oo <mj10050203@gmail.com>
2025-01-09 20:37:12 +0900

    fix login signup api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,13 +18,6 @@ export const Login = () => {
         };
 
         try {
-            const response = await fetch("http://localhost:5050/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(userData),
-            });
 
             const response = await fetch('http://localhost:5050/api/auth/login', {
                 method: 'POST',
